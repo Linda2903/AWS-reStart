71d3fe68e04bffc4e17cd8212a8c9669f502c206
Linda <lindagiannonea7@gmail.com>
2026-01-19 10:21:30 +0100

    Remove constants for URL configurations in quizzettone

--- quizzettone/costanti.py
+++
@@ -1,3 +0,0 @@
-BASE_URL: str = "https://raw.githubusercontent.com/linda2903/AWS-reStart/refs/heads/main/quizzettone"
-LISTA_DOMANDE_URL: str = f"{BASE_URL}/domande.txt"
-BASE_DOMANDA_SINGOLA_URL: str = f"{BASE_URL}/domande_risposte" 
