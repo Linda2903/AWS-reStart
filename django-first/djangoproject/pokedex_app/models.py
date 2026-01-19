df315d660c1d4ee0377fcd9f62517796df9911fe
Linda <lindagiannonea7@gmail.com>
2026-01-19 16:16:29 +0100

    Add Pokedex application with initial models, views, and admin configuration

---
+++ django-first/djangoproject/pokedex_app/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+class Pokemon(models.Model):
+    nome=models.CharField(max_length=100)
+    tipo=models.CharField(max_length=200)
+    peso=models.FloatField()
+    livello=models.IntegerField(default=1)
+    punti_salute=models.IntegerField()
+    descrizione=models.TextField(blank=True, null=True)
+    
+    def __str_(self):
+        return self.nome
